64b846c3d1f7bed98a7a3c3e50fe33899abff9b9
Danny <hiunji64@gmail.com>
2023-07-23 16:55:01 +0800

    feat: check miss image

--- db-check.ts
+++ db-check.ts
@@ -18,23 +18,25 @@ const main = async () => {
 async function findMissingCode(total: number) {
   try {
     let no = 1
+    let Miss_code: { missing_code: string }[] = []
     while (true) {
       const result: { missing_code: string }[] = await prisma.$queryRaw`
       --  if not exist will return 1
-      SELECT ${'NO' + String(no)} as  "missing_code" 
+      SELECT ${'NO' + checkNumberValue(no)} as  "missing_code" 
         FROM Image 
         WHERE NOT EXISTS (
           SELECT 1
           FROM Image
-          WHERE name = ${'NO' + String(no)}
+          WHERE name = ${'NO' + checkNumberValue(no)}
         )
       `
       if (result.length) {
-        console.log(result)
+        Miss_code.push(...result)
       }
       no++
       if (no === total) break
     }
+    console.log(removeDuplicatesObjArray(Miss_code))
     console.log('check finish')
   } catch (error) {
     console.error('Error:', error);
@@ -42,5 +44,18 @@ async function findMissingCode(total: number) {
     await prisma.$disconnect();
   }
 }
+const removeDuplicatesObjArray = (arr: { missing_code: string }[]) => {
+  let result = new Map()
+  for (let item of arr) {
+    result.set(item.missing_code, item)
+  }
 
+  return Array.from(result.values())
+}
+
+const checkNumberValue = (num: number) => {
+  if (num < 10 && num >= 0) return '00' + String(num)
+  if (num < 100 && num >= 10) return '0' + String(num)
+  return String(num)
+}
 findMissingCode(241)
